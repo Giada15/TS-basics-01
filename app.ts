09444dd0ef538686caf450dece04b491fb78cd1e
Giada15 <tascheriogiada@gmail.com>
2023-11-20 09:55:34 -0500

    Implement type enum in TypeScript

--- app.ts
+++ app.ts
@@ -2,16 +2,29 @@
 //   name: string;
 //   age: number;
 // } = {
-const person: {
-  name: string;
-  age: number;
-  hobbies: string[];
-  role: [number, string];
-} = {
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string];
+// } = {
+//   name: 'Lily',
+//   age: 25,
+//   hobbies: ['Sports', 'Cooking'],
+//   role: [2, 'author'],
+// };
+
+enum Role {
+  ADMIN,
+  READ_ONLY,
+  AUTHOR,
+}
+
+const person = {
   name: 'Lily',
   age: 25,
   hobbies: ['Sports', 'Cooking'],
-  role: [2, 'author'],
+  role: Role.ADMIN,
 };
 
 // person.role.push('admin');
@@ -25,4 +38,8 @@ console.log(person.name);
 
 for (const hobby of person.hobbies) {
   console.log(hobby.toLowerCase());
+}
+
+if (person.role === Role.AUTHOR) {
+  console.log('is author');
 }
